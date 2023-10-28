a7dca4336f4b7d87c307aebf65990c97cf12216b
busra.ayverdi <busraayverdi.95@gmail.com>
2023-10-28 22:18:26 +0300

    Add project files.

---
+++ RabbitMq.Subscriber/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMq.Subscriber
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri=new Uri("amqps://hkegsmzy:KAzxJXy8JOKCAO9JiQZrUz3vu1nX3xWC@fish" +
+                ".rmq.cloudamqp.com/hkegsmzy");
+
+            using var connection = factory.CreateConnection(); //Connection
+
+            var channel = connection.CreateModel(); // Kanal
+            channel.BasicQos(0, 1, false);
+            //channel.QueueDeclare("hello-queue", true, false, false); //durable-->true olmalı ki memeoride kuyruklar kalsın
+
+            var consumer=new EventingBasicConsumer(channel);
+            channel.BasicConsume("hello-queue",false,consumer);
+
+            consumer.Received+=(object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1500);
+
+                Console.WriteLine("Gelen mesaj" + message) ;
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+            
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
---
+++ RabbitMq.publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMq.publisher
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory=new ConnectionFactory();
+            factory.Uri=new Uri("amqps://hkegsmzy:KAzxJXy8JOKCAO9JiQZrUz3vu1nX3xWC@fish" +
+                ".rmq.cloudamqp.com/hkegsmzy");
+
+            using var connection = factory.CreateConnection() ; //Connection
+
+            var channel = connection.CreateModel(); // Kanal
+            channel.QueueDeclare("hello-queue", true,false,false); //durable-->true olmalı ki memeoride kuyruklar kalsın
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"Message {x}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+                Console.WriteLine($"Mesaj gönderilmiştir : {message}");
+            });
+
+           
+            Console.ReadLine();
+        
+        }
+    }
+}
